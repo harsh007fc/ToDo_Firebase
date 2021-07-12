726a6974abd52a502030a9d678fedc8e96492154
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-12 21:59:08 +0530

    basic structure done with hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todos, setTodos] = useState(['Do Work', 'HomeWork', 'Take dog for a walk']);
+  let [input, setInput] = useState('');
+
+
+  let addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-    <h1>hello </h1>
+      <h1>Lets Manage Our Work</h1>
+      <form >
+        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+        <button type='submit' onClick={addTodo}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
