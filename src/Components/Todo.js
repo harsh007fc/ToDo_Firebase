4686d9bdb17ec2e6d2aaf93f8590760d969f4c9b
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-13 22:21:26 +0530

    upto some styling

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import '../Css/Todo.css'
+import { List, ListItem, ListItemText, ImageIcon, Avatar, ListItemAvatar } from '@material-ui/core'
 
 function Todo(props) {
     return (
         <div>
-             <li>{props.todo}</li>
+            <List className='todo__list'>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={props.todo} secondary='Dummy Deadline 🕗 ' />
+                </ListItem>
+            </List>
         </div>
     )
 }
